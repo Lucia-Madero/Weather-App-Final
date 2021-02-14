b74bb6412231df2067a885bd4bc4599e13173a31
Lucia-Madero <78430885+Lucia-Madero@users.noreply.github.com>
2021-02-14 14:27:14 +0100

    Improved wind calculation

--- src/app.js
+++ src/app.js
@@ -56,6 +56,7 @@ function formatHours(timestamp) {
 }
 
 function showCity(response) {
+  console.log(response.data);
   title.innerHTML = response.data.name;
   mainTemp.innerHTML = Math.round(response.data.main.temp);
   currentCelsius = response.data.main.temp;
@@ -65,9 +66,9 @@ function showCity(response) {
   let humidity = Math.round(response.data.main.humidity);
   let currentHum = document.querySelector("#humidity");
   currentHum.innerHTML = `Humidity ${humidity} %`;
-  let wind = Math.round(response.data.wind.speed);
+  let wind = Math.round(response.data.wind.speed * 3, 6);
   let currentWind = document.querySelector("#wind");
-  currentWind.innerHTML = `Wind ${wind} %`;
+  currentWind.innerHTML = `Wind ${wind} km/h`;
   let icon = document.querySelector("#today-icon");
   icon.setAttribute(
     "src",
